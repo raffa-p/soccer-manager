2b0cb694f21c0ff5f73ef563b59c025cf4b20210
Raffaele Prota <95775115+raffa-p@users.noreply.github.com>
2026-04-16 17:21:13 +0000

    Risolto bug percorso '/'

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import Home from './Home';
-import Profilo from './Profilo';
-import CreaPartita from './CreaPartita';
-import ImpostaFormazione from './ImpostaFormazione';
-import VisualizzaFormazione from './VisualizzaFormazione';
-import InserisciRisultato from './InserisciRisultato';
-import ListaPartite from './ListaPartite';
-import ModificaPartita from './ModificaPartita';
-import Classifica from './Classifica';
-import ResetPassword from './ResetPassword';
-import ProtectedRoute from './ProtectedRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Home from "./Home";
+import Profilo from "./Profilo";
+import CreaPartita from "./CreaPartita";
+import ImpostaFormazione from "./ImpostaFormazione";
+import VisualizzaFormazione from "./VisualizzaFormazione";
+import InserisciRisultato from "./InserisciRisultato";
+import ListaPartite from "./ListaPartite";
+import ModificaPartita from "./ModificaPartita";
+import Classifica from "./Classifica";
+import ResetPassword from "./ResetPassword";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         {/* --- ROTTE PUBBLICHE --- */}
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<ResetPassword />} />
@@ -28,9 +29,18 @@ export default function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/profilo" element={<Profilo />} />
           <Route path="/crea-partita" element={<CreaPartita />} />
-          <Route path="/imposta-formazione/:id" element={<ImpostaFormazione />} />
-          <Route path="/visualizza-formazione/:id" element={<VisualizzaFormazione />} />
-          <Route path="/inserisci-risultato/:id" element={<InserisciRisultato />} />
+          <Route
+            path="/imposta-formazione/:id"
+            element={<ImpostaFormazione />}
+          />
+          <Route
+            path="/visualizza-formazione/:id"
+            element={<VisualizzaFormazione />}
+          />
+          <Route
+            path="/inserisci-risultato/:id"
+            element={<InserisciRisultato />}
+          />
           <Route path="/lista-partite" element={<ListaPartite />} />
           <Route path="/modifica-partita/:id" element={<ModificaPartita />} />
           <Route path="/classifica" element={<Classifica />} />
